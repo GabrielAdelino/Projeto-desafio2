cc0b3a153fb965cdb97ee794cc2e29264aa85bb3
Gabriel Adelino <gabriel.adelinodev@gmail.com>
2025-02-03 11:14:13 -0300

    ajustes tela menor

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // Gera um build estático
+  images: {
+    unoptimized: true, // Evita otimização de imagens, necessária para export estático
+  },
 };
 
 export default nextConfig;
